0581213b459307495eb1ed889a39f1f4c0d0bb9b
Tiago Simão <tiagosimao@gmail.com>
2017-09-18 18:37:42 +0100

    using docker for postgres tests

--- drowsy/src/test/java/org/irenical/drowsy/PostgresTest.java
+++ drowsy/src/test/java/org/irenical/drowsy/PostgresTest.java
@@ -1,47 +1,29 @@
 package org.irenical.drowsy;
 
-import static ru.yandex.qatools.embed.postgresql.distribution.Version.Main.PRODUCTION;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
 import org.irenical.jindy.Config;
 import org.irenical.jindy.ConfigFactory;
-import org.irenical.jindy.ConfigNotFoundException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-import ru.yandex.qatools.embed.postgresql.PostgresExecutable;
-import ru.yandex.qatools.embed.postgresql.PostgresProcess;
-import ru.yandex.qatools.embed.postgresql.PostgresStarter;
-import ru.yandex.qatools.embed.postgresql.config.AbstractPostgresConfig.Credentials;
-import ru.yandex.qatools.embed.postgresql.config.AbstractPostgresConfig.Net;
-import ru.yandex.qatools.embed.postgresql.config.AbstractPostgresConfig.Storage;
-import ru.yandex.qatools.embed.postgresql.config.AbstractPostgresConfig.Timeout;
-import ru.yandex.qatools.embed.postgresql.config.PostgresConfig;
-
-public class PostgresTest {
-
-  private static PostgresProcess postgresProcess;
+public abstract class PostgresTest {
   
-  private static PostgresConfig postgresConfig;
+  private static Config config = ConfigFactory.getConfig();
+  
+  @SuppressWarnings("rawtypes")
+  @ClassRule
+  public static PostgreSQLContainer postgres = new PostgreSQLContainer().withDatabaseName("test")
+          .withUsername(config.getString("jdbc.username")).withPassword(config.getString("jdbc.password"));
 
   @BeforeClass
-  public static void startPg() throws ClassNotFoundException, IOException, SQLException, ConfigNotFoundException {
-    Config config = ConfigFactory.getConfig();
-    Class.forName("org.postgresql.Driver");
-    PostgresStarter<PostgresExecutable, PostgresProcess> runtime = PostgresStarter.getDefaultInstance();
-
-    postgresConfig = new PostgresConfig(PRODUCTION, new Net("127.0.0.1", 54321), new Storage("test"), new Timeout(),
-        new Credentials(config.getMandatoryString("jdbc.username"), config.getString("jdbc.password")));
-
-    PostgresExecutable exec = runtime.prepare(postgresConfig);
-    postgresProcess = exec.start();
+  public static void startDb() throws Exception {
+      config.setProperty("jdbc.jdbcUrl", postgres.getJdbcUrl());
   }
-
+  
   @AfterClass
-  public static void shutdown() {
-    postgresProcess.stop();
+  public static void stopDb() {
+      postgres.stop();
   }
 
 }
